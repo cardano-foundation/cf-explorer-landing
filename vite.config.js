45b2ac18a5497f988ee1ee832e2a07524d0d0fe5
Roberto C. Morano <roberto.morano@cardanofoundation.org>
2024-08-14 19:45:13 +0200

    chore(vite): set correct base path for vercel

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/cf-explorer-landing/",
+  base: "/",
   plugins: [react()],
   server: {
     port: 3000, // Change if needed
